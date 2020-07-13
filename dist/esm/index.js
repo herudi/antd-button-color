5b8650a0b2094ded0c83ed567811f18e96055739
herudi <herudi7@gmail.com>
2020-07-13 16:15:17 +0700

    add index

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,2 +1,14 @@
-export * from './components';
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+import * as React from "react";
+import ButtonOriginal from "antd/lib/button/button";
+
+const Button = props => {
+  const _type = props.type;
+  return /*#__PURE__*/React.createElement(ButtonOriginal, _extends({}, props, {
+    type: _type
+  }));
+};
+
+export default Button;
 //# sourceMappingURL=index.js.map
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,16 @@
-export * from './components';
+import * as React from "react";
+import ButtonOriginal, { ButtonProps } from "antd/lib/button/button";
+
+type Mod<T, R> = Omit<T, keyof R> & R;
+type TButton = Mod<ButtonProps, {
+    type?: "default" | "primary" | "ghost" | "dashed" | "link" | "text" | "success" | "warning" | "info" | "dark" | "lightdark" | "danger"
+}>;
+
+const Button = (props: TButton) => {
+    const _type: any = props.type;
+    return (
+        <ButtonOriginal {...props} type={_type} />
+    )
+};
+
+export default Button;
